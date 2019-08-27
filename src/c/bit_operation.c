a7b74ac13c87ff30d33e75a4b2a9c362c45c6081
Vikas Sharma <kumarvikas786@gmail.com>
2019-08-27 17:12:54 +0530

    This commit is having the two function for bit-wise tweak. 	1. swap_bit in  bulk. 	2. reverse bits: from one to another position.
    code is tested and verfied my multiple interrartion.

--- src/c/bit_operation.c
+++ src/c/bit_operation.c
@@ -1,54 +1,80 @@
+#include <stdio.h>
 
-#define SETBIT(num,k) 		(num | (1<<k))
-#define CLEARBIT(num,k) 	(num & ~(1<<k))
-#define TOGGLEBIT(num,k) 	(num ^ (1<<k))
-#define SIZEOF(a) 			((char*)(&a+1)-(char*)&a)
+#define SETBIT(num, k) (num | (1 << k))
+#define CLEARBIT(num, k) (num & ~(1 << k))
+#define TOGGLEBIT(num, k) (num ^ (1 << k))
+#define SIZEOF(a) ((char *)(&a + 1) - (char *)&a)
 //number is 2^n
-#define POWER2N(num) 		(num & (num-1))
-//to-do another methods of 2^n
+#define POWER2N(num) (num & (num - 1)) // returns 0 for 2^n
 
+/*
+ *Get mask : it gives the mask for ith to jth position.
+ */
 
+#define GET_MASK(M, N) ((1U << ((M) - (N))) - 1)
 
-// if number is plaindrome or not
-// 1 -> success
-// 0 -> fail
-int ispalindrom_int(int num) {
-	char buf[100] = {0};
-	sprintf(buf,"%d",num);
-	int i=0,j=0,len=0;
-	len = strlen(buf)-1;
-	while(i<j){
-		if(buf[i]!=buf[j]){
-			printf("Given number is not plaindrom\n");
-			return 0;
+/* 
+ * Swap bits from i to j position for n consecative bits for given number.
+ * i,j: position from where it start swapping.
+ * n  : number of consecative bits.
+ * b  : Given number
+ * r  : result to return.
+ * x  : temp varriable.  
+ */
+
+unsigned int swap_bits(unsigned int b, int i, int j, int n)
+{
+
+	unsigned int mask = ((1U << n) - 1);
+	//printf("mask:0x%X\n", mask);
+	unsigned int x = (((b >> i) ^ (b >> j)) & mask);
+	//printf("x: 0x%X\n", x);
+	unsigned int r = b ^ ((x << i) | (x << j));
+
+	return r;
+}
+/*
+ * Reverse bits: 
+ */
+
+int reverse_bits(unsigned int num, int from_bit, int to_bit)
+{
+
+	int i = 0;
+	int mask = GET_MASK(to_bit,from_bit);
+	printf("mask: %X\n",mask);
+	unsigned int res = 0;
+	for (i = from_bit; i <= to_bit -1 ; i++)
+	{
+		if (num & (1 << i))
+		{
+			res |= (1UL << ((to_bit -1)-i));
 		}
 	}
-	return 1;
-}
-int ispalindrom_str(char* str) {
-	int i=0,j=0,len=0;
-	len = strlen(str)-1;
-	while(i<j){
-		if(str[i]!=str[j]){
-			printf("Given string:%s is not plaindrom\n",str);
-			return 0;
-		}
-	}
-	return 1;
+	//printf("%d res %X\n",__LINE__,res);
+	return (res << from_bit) |(~(mask << from_bit) & num);
 }
 
-// if number is prime or not
-// 1 -> success
-// 0 -> fail
-
-int isprime(int n){
-	if(n < 0) n = -n;
-	if(n>=0 & n<2) print("number is neither prime nor composite\n");
-	int i=2;
-	for(i=2;i<n/2;i++){
-		if(n%i==0){
-			return 1;
-		}
+void diplay_binary(int a)
+{
+	if (a > 1)
+	{
+		diplay_binary(a);
 	}
-	return 0;
+	printf("%d", a % 2);
 }
+#if 0
+void main()
+{
+	unsigned int n = 0xABCD98EF;
+	
+	printf("%d n: 0x%X\n", __LINE__, n);
+	int res = swap_bits(n, 0, 24, 8);
+	printf("res: 0x%X\n", res);
+	res = swap_bits(n, 8, 16, 8);
+	printf("res: 0x%X\n", res);
+	res = reverse_bits(n, 0, 27);
+	printf("%d res: 0x%X\n", __LINE__, res);
+	return;
+}
+#endif
--- src/c/bit_test.c
+++ src/c/bit_test.c
@@ -1,7 +1,55 @@
 #include <stdio.h>
 
 #define dprintf(fmt,...) do{ printf(" %s %d " fmt,__FILE__,__LINE__,__VA_ARGS__)\
-														;}while(0);
+	;}while(0);
+
+/* checking the division.*/
+
+// function to convert decimal to binary 
+void decToBinary(int n) 
+{ 
+	// array to store binary number 
+	int val = n;
+	int binaryNum[32]; 
+
+	// counter for binary array 
+	int i = 0; 
+	while (n > 0) { 
+		// storing remainder in binary array 
+		binaryNum[i] = n % 2; 
+		printf("n = %d\t rem = %d\n",n,binaryNum[i]);
+		n = n / 2; 
+		i++; 
+	} 
+
+	// printing binary array in reverse order 
+	printf("%d = ",val);
+	for (int j = i - 1; j >= 0; j--) 
+		printf("%d", binaryNum[j]); 
+
+	printf("\n");
+} 
+
+
+void div(int n){
+
+	int count = 0,rem = 0;
+	int power = 1; 
+
+//	printf("Count: %d\tNumber: %d\t Remaider: %d\n",count,n,rem);
+	while(n){
+		rem = n%2;
+		printf("Count: %d\tNumber: %d\t Remaider: %d\n",count,n,rem);
+		n =n/2;
+		count++;
+		power = 2 * power ;
+	}
+	printf(" power = %d\n",power );
+	printf("=======================================\n");
+}
+
+
+
 
 // now Check interger overflow.
 void int_overflow(){
@@ -28,8 +76,9 @@ void int_overflow(){
 //==============================================================================
 // using bit wise operator.
 // Worst case: O(no of bits)
-int find_count(int n){
+int find_count(int val){
 	unsigned int n, count = 0;
+	n = val;
 	while (n){
 		if(n & 1){
 			count++;
@@ -41,7 +90,7 @@ int find_count(int n){
 
 // using modulo oprator 
 // complexity : worst case = O( no of bits)
-int find_count(int val){
+int find_count_m(int val){
 	unsigned int n = val, count = 0;
 	while (n) {
 		if (n/2 ==1){
@@ -54,7 +103,7 @@ int find_count(int val){
 // using toggling approch 
 // complexity : worst case = O( no of set bits)
 int find_counts(int val){
-	unsigned int n = val, count = 0;
+	unsigned int n = val; int  count = 0;
 	while (n){
 		count++;
 		n = n & (n-1);
@@ -63,8 +112,9 @@ int find_counts(int val){
 }
 // using Group Processing approch 
 // complexity : worst case = O(bits/4)
-int table[] = {0,1,1,2,1,2,2,3,1,2,2,3,2,3,3,4}
-int find_counts(int val){
+
+int table[] = {0,1,1,2,1,2,2,3,1,2,2,3,2,3,3,4};
+int find_counts_l(int val){
 	unsigned int n = val, count = 0;
 	while (n){
 		count = count + table[(n & 0x0F)];
@@ -72,11 +122,61 @@ int find_counts(int val){
 	}
 	return count;
 }
+// if number is plaindrome or not
+// 1 -> success
+// 0 -> fail
+int ispalindrom_int(int num) {
+	char buf[100] = {0};
+	sprintf(buf,"%d",num);
+	int i=0,j=0,len=0;
+	len = strlen(buf)-1;
+	while(i<j){
+		if(buf[i]!=buf[j]){
+			printf("Given number is not plaindrom\n");
+			return 0;
+		}
+	}
+	return 1;
+}
+int ispalindrom_str(char* str) {
+	int i=0,j=0,len=0;
+	len = strlen(str)-1;
+	while(i<j){
+		if(str[i]!=str[j]){
+			printf("Given string:%s is not plaindrom\n",str);
+			return 0;
+		}
+	}
+	return 1;
+}
 
+// if number is prime or not
+// 1 -> success
+// 0 -> fail
 
+int isprime(int n){
+	if(n < 0) n = -n;
+	if(n>=0 & n<2) print("number is neither prime nor composite\n");
+	int i=2;
+	for(i=2;i<n/2;i++){
+		if(n%i==0){
+			return 1;
+		}
+	}
+	return 0;
+
+}
 //==============================================================================
 int main(){
-	
- int_overflow();
+
+	//	int_overflow();
+	decToBinary(12);
+	div(12);
+	decToBinary(13);
+	div(13);
+	decToBinary(16);
+	div(16);
+	decToBinary(15);
+	div(15);
 	return 0;
 }
